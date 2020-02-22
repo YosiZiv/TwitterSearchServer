1c70a58c6f2c6b1169f8f5375df365acf16b2e5a
katlany <notan8@gmail.com>
2020-02-22 18:23:53 +0200

    server init

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const api = require("./api/api");
+const app = express();
+
+//  Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(cors());
+app.use("/api", api);
+//  API
+// app.use("/api", api);
+
+// Global error handler
+// process.on('unhandledRejection', ex => {
+//   throw ex;
+// });
+
+const port = process.env.PORT || 4000;
+app.use(express.static("public/build"));
+app.get("*", (req, res) => {
+  if (req.path === "/api") {
+    console.log("Check!223");
+    next();
+  }
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
