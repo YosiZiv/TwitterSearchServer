f96aec2a301375cf2d08b3b79219a2becea95b05
katlany <notan8@gmail.com>
2020-02-25 00:51:08 +0200

    api getembed commet out and well remove in feasture

--- api/handlers/twitter.js
+++ api/handlers/twitter.js
@@ -1,40 +1,60 @@
-const axios = require("axios");
+const { api } = require("../../httpConfig");
 exports.getTweetsByHashtags = async (req, res, next) => {
-  const {
-    headers: { authorization },
-    body: { hashtags }
-  } = req;
-  // console.log(req.body, "function work");
-  const response = await axios.get(
-    `https://api.twitter.com/1.1/search/tweets.json?q=%23${hashtags}`,
-    {
-      headers: {
-        authorization
-      }
-    }
-  );
-  const filteredTweets = [];
-  const allowed = ["text", "user", "created_at", "retweet_count"];
-  const tweets = response.data.statuses;
-  tweets.forEach(tweet => {
-    // console.log("MAP FUNCTION LOG ", tweet);
-    const filteredObject = Object.keys(tweet)
-      .filter(key => allowed.includes(key))
-      .reduce((obj, key) => {
-        if (key === "user") {
-          obj[key] = tweet[key].name;
-          return obj;
-        } else {
-          obj[key] = tweet[key];
-          return obj;
-        }
-      }, {});
-    filteredTweets.push(filteredObject);
-  });
-  console.log(filteredTweets);
+  const { hashtags } = req.body;
+  console.log(hashtags, "function work");
+  try {
+    const twitterReq = {
+      url: `https://api.twitter.com/1.1/search/tweets.json?q=%3A${hashtags}`,
+      method: "GET"
+    };
+    const response = await api(twitterReq);
+    // if(response.data.statuses)
+    console.log("RESPONSE DATA ", response.data);
 
-  return res.status(200).json({ tweets, filteredTweets });
+    const filteredTweets = [];
+    const allowed = ["id_str", "text", "user", "created_at", "retweet_count"];
+    const tweets = response.data.statuses;
+    tweets.forEach(tweet => {
+      // console.log("MAP FUNCTION LOG ", tweet);
+      const filteredObject = Object.keys(tweet)
+        .filter(key => allowed.includes(key))
+        .reduce((obj, key) => {
+          if (key === "user") {
+            obj[key] = tweet[key].name;
+            obj["screen_name"] = tweet[key].screen_name;
+            return obj;
+          } else {
+            obj[key] = tweet[key];
+            return obj;
+          }
+        }, {});
+      filteredTweets.push(filteredObject);
+    });
+    console.log(tweets);
+
+    return res.status(200).json({ tweets, filteredTweets });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
+// exports.getTwitterEmbed = async (req, res, next) => {
+//   const { screen_name, id_str } = req.body;
+//   console.log(screen_name, id_str);
+//   try {
+//     const twitterReq = {
+//       url: `https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2F${screen_name}%2Fstatus%2F${id_str}`,
+//       method: "GET"
+//     };
+//     const response = await api(twitterReq);
+//     const tweet = response.data.html;
+//     console.log(response.data.html);
+
+//     return res.status(200).json({ tweet });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 // console.log(filterdObject);
 // const resultAllow =
 // console.log("INSIDE FILTER FUNCTION", key, resultAllow);
--- api/routes/twitter.js
+++ api/routes/twitter.js
@@ -3,8 +3,8 @@ const express = require("express");
 // initalizing express router
 const router = express.Router();
 
-const { getTweetsByHashtags } = require("../handlers/twitter");
+const { getTweetsByHashtags, getTwitterEmbed } = require("../handlers/twitter");
 
 router.post("/tweets", getTweetsByHashtags);
-
+// router.post("/getembed", getTwitterEmbed);
 module.exports = router;
