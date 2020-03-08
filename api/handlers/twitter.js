a133edb9e69d4c3ca099e5949e51734f70fc16d7
katlany <notan8@gmail.com>
2020-03-08 02:44:05 +0200

    refactore handler add sub handler for filter

--- api/handlers/twitter.js
+++ api/handlers/twitter.js
@@ -1,17 +1,39 @@
 const { api } = require("../../httpConfig");
+
+const filterArrayOfObjects = (array, filters) => {
+  const filteredArray = []; // INIT EMPTY ARRAY FOR FILTER KEYS TWEETS OBJECTS
+  array.forEach(item => {
+    const filteredObject = Object.keys(item)
+      .filter(key => {
+        return filters.includes(key);
+      })
+      .reduce((obj, key) => {
+        if (key === "user") {
+          // if key is user give only user name a bit of hack+ but work for now
+          obj[key] = item[key].name;
+          return obj;
+        }
+        obj[key] = item[key];
+        return obj;
+      }, {});
+    filteredArray.push(filteredObject);
+  });
+  return filteredArray;
+};
+
 exports.getTweetsByHashtag = async (req, res, next) => {
   const {
     hashtag = "",
     startDate = "",
     endDate = "",
     language = "",
-    reqData = null
-  } = req.body;
-  console.log(hashtag, language);
+    filters = null
+  } = req.query;
+  console.log("FUNCTION HIT", hashtag, startDate, endDate, language);
 
   try {
     let params = "";
-    const hashtagUrl = `%23${hashtag.value}`;
+    const hashtagUrl = `%23${hashtag}`;
     const startDateUrl = `+since%3A${startDate}`;
     let endDateUrl = `+until%3A${endDate}`;
     let languageUrl = `&lang=${language}`;
@@ -23,37 +45,24 @@ exports.getTweetsByHashtag = async (req, res, next) => {
         language ? languageUrl : ""
       );
     }
+    console.log("FUNCTION HIT", params);
     const twitterReq = {
       url: `https://api.twitter.com/1.1/search/tweets.json?q=` + params,
       method: "GET"
     };
-    console.log(twitterReq, language);
+    console.log(twitterReq.url);
+
     const response = await api(twitterReq);
-    const tweets = response.data.statuses;
-    const filteredTweets = []; // INIT EMPTY ARRAY FOR FILTER KEYS TWEETS OBJECTS
-
-    // THIS ABOVE LINE ONLY NEED IN CLIENT REQUIRE FILTERED DATA
-    if (tweets.length && reqData && typeof reqData === "object") {
-      tweets.forEach(tweet => {
-        const filteredObject = Object.keys(tweet)
-          .filter(key => reqData.includes(key))
-          .reduce((obj, key) => {
-            if (key === "user") {
-              obj[key] = tweet[key].name;
-              obj["screen_name"] = tweet[key].screen_name;
-              return obj;
-            } else {
-              obj[key] = tweet[key];
-              return obj;
-            }
-          }, {});
-        filteredTweets.push(filteredObject);
-      });
+    const { statuses } = response.data; // twitter api give twitter response in statues key
+    let tweets = statuses;
+    if (statuses.length && filters && typeof filters === "object") {
+      console.log("if check hit");
+      tweets = filterArrayOfObjects(statuses, filters);
     }
-    return res.status(200).json({ tweets, filteredTweets });
-  } catch (err) {
-    console.log(err);
+    console.log(tweets);
 
+    return res.status(200).json({ tweets });
+  } catch (err) {
     return res.status(400).json({ errors: "somting went wrong :/", err });
   }
 };
